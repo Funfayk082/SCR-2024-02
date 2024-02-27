6c200b390fe0f7392e7e4bf423418d79c099c484
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2024-02-27 19:24:55 +0300

    feat: Реализовал методы sum, scale, axpy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,18 @@
-
+import module1.homework.LinearAlgebraOps
 
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
+    var res1, res2 = Array[Int](1, 2)
+    res1(0) = 2
+    res1(1) = 5
+    res2(0) = 14
+    res2(1) = 1
+    val a = 5
+    val result = LinearAlgebraOps.axpy(a, res1, res2)
+    println(result(0), result(1))
   }
 }
 
